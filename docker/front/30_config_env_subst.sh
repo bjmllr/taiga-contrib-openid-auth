324b75f1585143c58d400ec30fab64f546b79af0
FallenChromium <43214067+FallenChromium@users.noreply.github.com>
2024-04-05 16:52:21 +0300

    Update template files to v6.7.7 version of taiga-front

--- docker/front/30_config_env_subst.sh
+++ docker/front/30_config_env_subst.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-# Copyright (C) 2014-present Taiga Agile LLC
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+# Copyright (c) 2021-present Kaleidos Ventures SL
 
 contribs=()
 
@@ -24,11 +14,7 @@ if [[ -z "${ENABLE_SLACK}" ]]; then
 fi
 
 if [ ${ENABLE_SLACK} == "true" ]; then
-    contribs+=('"/plugins/slack/slack.json"')
-fi
-
-if [ ${ENABLE_OPENID} == "true" ]; then
-    contribs+=('"/plugins/openid-auth/openid-auth.json"')
+    contribs+=('"plugins/slack/slack.json"')
 fi
 
 # Public registration and oauth
@@ -38,11 +24,15 @@ fi
 
 if [ ${PUBLIC_REGISTER_ENABLED} == "true" ]; then
     if [ ${ENABLE_GITHUB_AUTH} == "true" ]; then
-        contribs+=('"/plugins/github-auth/github-auth.json"')
+        contribs+=('"plugins/github-auth/github-auth.json"')
     fi
     if [ ${ENABLE_GITLAB_AUTH} == "true" ]; then
-        contribs+=('"/plugins/gitlab-auth/gitlab-auth.json"')
+        contribs+=('"plugins/gitlab-auth/gitlab-auth.json"')
     fi
+fi
+
+if [ ${ENABLE_OPENID} == "true" ]; then
+    contribs+=('"/plugins/openid-auth/openid-auth.json"')
 fi
 
 # Importers
@@ -58,6 +48,16 @@ if [[ -z "${ENABLE_TRELLO_IMPORTER}" ]]; then
     export ENABLE_TRELLO_IMPORTER="false"
 fi
 
+# Default language
+if [[ -z "${DEFAULT_LANGUAGE}" ]]; then
+    export DEFAULT_LANGUAGE="en"
+fi
+
+# Debug
+if [[ -z "${DEBUG}" ]]; then
+    export DEBUG="false"
+fi
+
 contribs=$( IFS=,; echo "[${contribs[*]}]" )
 
 export CONTRIB_PLUGINS=$contribs
@@ -67,3 +67,5 @@ if [ ! -f "$FILE" ]; then
     envsubst < /usr/share/nginx/html/conf.json.template \
              > /usr/share/nginx/html/conf.json
 fi
+
+sed -i 's;<base href="/">;<base href="'"${TAIGA_SUBPATH}/"'">;g' /usr/share/nginx/html/index.html
